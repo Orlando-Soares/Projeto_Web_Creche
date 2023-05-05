c66ce2d73807d9131415a62844baaa14372464d1
Orlando Soares <orlando.almeida.soares@gmail.com>
2023-05-05 11:20:00 -0300

    Implementei variaveis de ambiente e envio de email

---
+++ autoload.php
@@ -0,0 +1,22 @@
+<?php
+    spl_autoload_register(
+        function($class) {
+            $file = __DIR__ . "/" . $class . ".php";
+            $file = str_replace("\\", "/", $file);
+            var_dump($file);
+            if(file_exists($file) && !is_dir($file)) {
+                include $file;                
+            } else {
+                
+                $file = __DIR__ . "/Views/" . $class . ".php";
+                $file = str_replace("\\", "/", $file);
+                
+                if(file_exists($file) && !is_dir($file)) {
+                    include $file;                
+                }
+                
+            }
+            
+        }
+    );
+?>
